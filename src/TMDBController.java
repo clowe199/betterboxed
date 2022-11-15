3fe2a7939714b9458afc1cb4a6d5a95cf1d71e0b
Jacob <jakesoto48@gmail.com>
2022-11-15 17:57:50 -0600

    Update TMDBController.java

--- src/TMDBController.java
+++ src/TMDBController.java
@@ -1,16 +1,28 @@
 import info.movito.themoviedbapi.TmdbAccount;
 import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.TmdbAuthentication;
-import info.movito.themoviedbapi.TmdbMovies;
+
 
 public class TMDBController {
-    
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        String apiKey = "7f8fa1bf325f4325f96ae5abae237bd1";
-        String apiRAToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjhmYTFiZjMyNWY0MzI1Zjk2YWU1YWJhZTIzN2JkMSIsInN1YiI6IjYzNDYyMGRjNjk5ZmI3MDA3OTA3Mjk2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yhkymI1CqH9Mp4UMS3Fd_-Kx2Jw9p-zIEx4N5WelR8A";
+    private TmdbApi apiKey;
+    private TmdbAccount userAccount;
+    private String key = "7f8fa1bf325f4325f96ae5abae237bd1";
+    private String query;
 
-        TmdbAccount movies = new TmdbApi(apiKey).getAccount();
-        //System.out.println(movies);
+    public void TMDBController(String key){
+        apiKey = new TmdbApi(key);
+    }
+
+    public void getMovieData(String movieId){
+        query = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key="
+        + key + "&language=en-US";
+        System.out.println(query);
+        response = requests.get(query);
+    }
+
+    
+
+    public static void main(String args[]){
+        TMDBController myController = new TMDBController();
+        myController.getMovieData("464052");
     }
 }
