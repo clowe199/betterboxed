dc587af1aea14560ef334c3f5cddc75200fd861b
Kyle Lashmet <kylelashmet9@gmail.com>
2022-11-30 21:56:02 -0600

    added the new methods

--- src/main/java/SQLDBConnector.java
+++ src/main/java/SQLDBConnector.java
@@ -1,48 +1,50 @@
 import java.sql.*;
 import java.util.*;
 
+
 public class SQLDBConnector
 {
     private static final String DB_URL = "jdbc:mysql://localhost/BetterBox";
     private static final String USER = "betteruser";
     private static final String PASS = "password";
-   // get users return array of userdatas
 
-   //  public static void main(String[] args)
-   //  {
-   //      ArrayList<String[]> sensitiveInfo = getSensitiveInfo();
+    public static void main(String[] args)
+    {
+        // insertUser("kyle", "mypassword");
+        //insertWatchedLater("kyle", 123456);
 
-   //      for(String[] user : sensitiveInfo){
-   //          System.out.println(user[0] + " - " + user[1]);
-   //      }
+       //insertUser("kyle", "mypassword");
+        // insertWatchedLater("kyle", 175854);
+        // insertWatchedLater("kyle", 47852);
+        // insertWatchedLater("kyle", 378527);
+        // insertWatchedLater("kyle", 12345);
+        // ArrayList<String[]> sensitiveInfo = getWatchLater("kyle");
 
-   //      insertUser("kyle", "mypassword");
-   //      sensitiveInfo = getSensitiveInfo();
+        // for(String[] movie : sensitiveInfo){
+        //     System.out.println(movie[0]);
+        // }
+    }
 
-   //      for(String[] user : sensitiveInfo){
-   //          System.out.println(user[0] + " - " + user[1]);
-   //      }
-   //  }
+    // public static ArrayList<String[]> getSensitiveInfo(){
+    //     ArrayList<String[]> sens = new ArrayList<String[]>();
+    //     try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    //      Statement stmt = conn.createStatement();
 
-   //  public static ArrayList<String[]> getSensitiveInfo(){
-   //      ArrayList<String[]> sens = new ArrayList<String[]>();
-   //      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-   //       Statement stmt = conn.createStatement();
-   //        ResultSet rs = stmt.executeQuery(QUERY);) {
-   //        Extract data from result set
-   //       while (rs.next()) {
-   //          String[] curr = new String[2];
-   //          curr[0] = rs.getString("username");
-   //          curr[1] = rs.getString("userPassword");
-   //          sens.add(curr);
-   //       }
+    //       ResultSet rs = stmt.executeQuery(QUERY);) {
+    //       //Extract data from result set
+    //      while (rs.next()) {
+    //         String[] curr = new String[2];
+    //         curr[0] = rs.getString("username");
+    //         curr[1] = rs.getString("userPassword");
+    //         sens.add(curr);
+    //      }
          
-   //    } catch (SQLException e) {
-   //       e.printStackTrace();
-   //    } 
-   //    return sens;
-   //  }
-
+    //   } catch (SQLException e) {
+    //      e.printStackTrace();
+    //   } 
+    //   return sens;
+    // }
+    
     public static int insertUser(String username, String password){
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
          CallableStatement cstmt = conn.prepareCall("{? = call insert_user(?,?)}");
@@ -56,6 +58,141 @@ public class SQLDBConnector
          e.printStackTrace();
       } 
       return -1;
+    }
+
+    public static int insertWatchedLater(String username, int movieID)
+    {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS))
+        {
+            CallableStatement cstmt = conn.prepareCall("{? = call insert_watch_later(?,?)}");
+            cstmt.registerOutParameter(1, Types.INTEGER);
+            cstmt.setString(2, username);
+            cstmt.setInt(3, movieID);
+            cstmt.execute();
+            int returnCode = cstmt.getInt(1);
+            return returnCode;
+
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return -1; 
+    }
+    
+    public static ArrayList<String[]> getWatchLater(String user)
+    {
+        final String QUERY = "SELECT movieid from watchlater where username = '" + user +"'";
+        System.out.println(QUERY);
+        ArrayList<String[]> movies = new ArrayList<String[]>();
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt =  conn.createStatement();
+        ResultSet rs  = stmt.executeQuery(QUERY);)
+        {
+            
+            while(rs.next())
+            {
+                System.out.println(rs.getString("movieid"));
+                String[] curr = new String[1];
+                curr[0] = rs.getString("movieid");
+                movies.add(curr);
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    public static int insertWatched(String username, int movieID)
+    {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS))
+        {
+            CallableStatement cstmt = conn.prepareCall("{? = call insert_watched(?,?)}");
+            cstmt.registerOutParameter(1, Types.INTEGER);
+            cstmt.setString(2, username);
+            cstmt.setInt(3, movieID);
+            cstmt.execute();
+            int returnCode = cstmt.getInt(1);
+            return returnCode;
+
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return -1; 
+    }
+
+    public static ArrayList<String[]> getWatched(String user)
+    {
+        final String QUERY = "SELECT movieid from watchedmovies where username = '" + user +"'";
+        System.out.println(QUERY);
+        ArrayList<String[]> movies = new ArrayList<String[]>();
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt =  conn.createStatement();
+        ResultSet rs  = stmt.executeQuery(QUERY);)
+        {
+            
+            while(rs.next())
+            {
+                System.out.println(rs.getString("movieid"));
+                String[] curr = new String[1];
+                curr[0] = rs.getString("movieid");
+                movies.add(curr);
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    public static int insertSaved(String username, int movieID)
+    {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS))
+        {
+            CallableStatement cstmt = conn.prepareCall("{? = call insert_saved(?,?)}");
+            cstmt.registerOutParameter(1, Types.INTEGER);
+            cstmt.setString(2, username);
+            cstmt.setInt(3, movieID);
+            cstmt.execute();
+            int returnCode = cstmt.getInt(1);
+            return returnCode;
+
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return -1; 
+    }
+
+    public static ArrayList<String[]> getSavedMovies(String user)
+    {
+        final String QUERY = "SELECT movieid from savedmovies where username = '" + user +"'";
+        System.out.println(QUERY);
+        ArrayList<String[]> movies = new ArrayList<String[]>();
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt =  conn.createStatement();
+        ResultSet rs  = stmt.executeQuery(QUERY);)
+        {
+            
+            while(rs.next())
+            {
+                System.out.println(rs.getString("movieid"));
+                String[] curr = new String[1];
+                curr[0] = rs.getString("movieid");
+                movies.add(curr);
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return movies;
     }
 }
 
