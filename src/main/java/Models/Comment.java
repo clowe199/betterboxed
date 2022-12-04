1947b51479da3fb12dd6b7428d67b1a4973e188e
Jacob <jakesoto48@gmail.com>
2022-12-04 03:05:55 -0600

    Attempting Builder Class

--- src/main/java/Models/Comment.java
+++ src/main/java/Models/Comment.java
@@ -2,13 +2,22 @@ package Models;
 import java.util.List;
 
 public class Comment extends Rating {
-    private int ratingId;
+    private String ratingId;
     private String content;
     private int numLikes;
     private int numDislikes;
 
 
-    public Comment(int ratingId, int rating, int movieId, String userName, String content, int numLikes, int numDislikes){
+    /*
+    public Comment(CommentBuilder builder){
+        this.content = builder.content;
+        this.numLikes = builder.numLikes;
+        this.numDislikes = builder.numDislikes;
+        super();
+    }
+*/
+
+    public Comment(String ratingId, int rating, int movieId, String userName, String content, int numLikes, int numDislikes){
         super(ratingId, rating, movieId, userName);
         this.content = content;
         this.numLikes = numLikes;
@@ -16,7 +25,7 @@ public class Comment extends Rating {
     }
 
     /* Getter Methods */
-    public int getRatingId() {
+    public String getRatingId() {
         return ratingId;
     }
 
@@ -34,7 +43,7 @@ public class Comment extends Rating {
 
 
     /* Setter Methods */
-    public void setRatingId(int ratingId) {
+    public void setRatingId(String ratingId) {
         this.ratingId = ratingId;
     }
 
@@ -70,5 +79,48 @@ public class Comment extends Rating {
         //create comment objects for each one and add them to return list.
         return null;
     }*/
+
+
+    /* Builder Class */
+    /*
+    static public class CommentBuilder extends Rating.RatingBuilder{
+        private int ratingId;
+        private String content;
+        private int numLikes;
+        private int numDislikes;
+        private int rating;
+        private int movieId;
+        private String userName;
+
+        public RatingBuilder ratingId(int id)
+        {
+            this.ratingId = id;
+            return this;
+        }
+        
+        public RatingBuilder content(String comment)
+        {
+            this.content = comment;
+            return this;
+        }
+
+        public RatingBuilder numLikes(int likes)
+        {
+            this.numLikes = likes;
+            return this;
+        }
+
+        public RatingBuilder numDislikes(int dislikes)
+        {
+            this.numDislikes = dislikes;
+            return this;
+        }
+
+        public Comment build()
+        {
+            return new Comment(this);
+        }
+    }*/
+
 
 }
