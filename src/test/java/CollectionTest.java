8a0c70c9d717bed9aefa2cc66d1cc4802042aa12
DeMarco King <dcking4@ilstu.edu>
2022-12-03 20:05:31 -0600

    commented out jupiter on tests Moved SQLDB to its own folder

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -5,9 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-
-import org.junit.Test;
-
 public class CollectionTest extends TestConstants {
 
     //probably will add some kind of before and after
--- src/test/java/UserDataTest.java
+++ src/test/java/UserDataTest.java
@@ -1,11 +1,13 @@
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class UserDataTest extends TestConstants {
     private UserData data;
