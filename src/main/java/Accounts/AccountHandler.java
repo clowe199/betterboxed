a71ac9ff67a12717e59d7a979a379bc2641916cd
Natonk-Brose <tallkid02@gmail.com>
2022-12-06 03:52:31 -0600

    leave review

--- src/main/java/Accounts/AccountHandler.java
+++ src/main/java/Accounts/AccountHandler.java
@@ -165,6 +165,24 @@ public class AccountHandler {
         goToMovieSearch();
     }
 
+    //movieFoundMenu
+    private void leaveReview(int movieId){
+        System.out.println("Enter number rating: ");
+        String rating = scan.nextLine();
+        int ratingInt;
+        try {
+            ratingInt = Integer.parseInt(rating);
+            System.out.println("Enter review content: ");
+            String content = scan.nextLine();
+            userAccount.addReview(content, ratingInt, movieId);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid rating");
+            leaveReview(movieId);
+            return;
+        }
+        movieFoundMenu(movieId);
+    }
+
     // Save movie to collection
     private void saveMovie(int id){
         System.out.println("Save movie: "+ userAccount.getMovieData(id).getOverview());
