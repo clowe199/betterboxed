9313c950b4df0da5e4db9fc1b423b2fa033495a4
Natonk-Brose <tallkid02@gmail.com>
2022-12-05 18:29:37 -0600

    Collection and UserData tests

--- src/test/java/UserDataTest.java
+++ src/test/java/UserDataTest.java
@@ -1,6 +1,9 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,10 +16,8 @@ public class UserDataTest extends TestConstants {
 
     @Before
     public void setup(){
-        this.data = new UserData.UserDataBuilder()
-        		// .password(TEST_PASSWORD)
+        data = new UserData.UserDataBuilder()
         		.username(TEST_USERNAME)
-        		// .emailAddress(TEST_ADDRESS)
         		.build();
     }
 
@@ -28,47 +29,85 @@ public class UserDataTest extends TestConstants {
     @Test
     public void testNewUserData(){
         setup();
-        // assertEquals(TEST_ADDRESS, data.getEmailAddress());
-        // assertArrayEquals(TEST_PASSWORD, data.getPassword());
         assertEquals(TEST_USERNAME, data.getUsername());
         assertEquals(UserData.WATCHED_TITLE, data.getWatchedList().getName());
         assertEquals(UserData.WATCH_LATER_TITLE, data.getWatchLaterList().getName());
     }
 
     @Test
+    public void testAddToWatched(){
+        setup();
+        assertFalse(data.getWatchedList().contains(TEST_MOVIE_ID));
+        data.addToWatched(TEST_MOVIE_ID);
+        assertTrue(data.getWatchedList().contains(TEST_MOVIE_ID));
+    }
+    @Test
     public void testAddAndRemoveToWatched(){
         setup();
-        // Movie m = new Movie.MovieBuilder()
-        //     .movieId(TEST_MOVIE_ID)
-        //     .movieName(TEST_TITLE)
-        //     .releaseDate(TEST_RELEASE_DATE)
-        //     .averageRating(TEST_AVG_RATING)
-        //     .build();
+        assertFalse(data.watchedContains(TEST_MOVIE_ID));
         data.addToWatched(TEST_MOVIE_ID);
         assertTrue(data.watchedContains(TEST_MOVIE_ID));
         data.removeFromWatched(TEST_MOVIE_ID);
         assertFalse(data.watchedContains(TEST_MOVIE_ID));
     }
+    @Test
+    public void testWatchedContains(){
+        setup();
+        assertFalse(data.watchedContains(TEST_MOVIE_ID));
+        data.addToWatched(TEST_MOVIE_ID);
+        assertTrue(data.watchedContains(TEST_MOVIE_ID));
+    }
 
+    @Test
+    public void testAddToWatchLater(){
+        setup();
+        data.addToWatchLater(TEST_MOVIE_ID);
+        assertTrue(data.watchLaterContains(TEST_MOVIE_ID));
+    }
     @Test
     public void testAddAndRemoveToWatchLater(){
         setup();
-        // Movie m = new Movie.MovieBuilder()
-        //     .movieId(TEST_MOVIE_ID)
-        //     .movieName(TEST_TITLE)
-        //     .releaseDate(TEST_RELEASE_DATE)
-        //     .averageRating(TEST_AVG_RATING)
-        //     .build();        
         data.addToWatchLater(TEST_MOVIE_ID);
         assertTrue(data.watchLaterContains(TEST_MOVIE_ID));
         data.removeFromWatchLater(TEST_MOVIE_ID);
         assertFalse(data.watchLaterContains(TEST_MOVIE_ID));
     }
+    @Test
+    public void testWatchLaterContains(){
+        setup();
+        assertFalse(data.getWatchLaterList().contains(TEST_MOVIE_ID));
+        data.addToWatchLater(TEST_MOVIE_ID);
+        assertTrue(data.getWatchLaterList().contains(TEST_MOVIE_ID));
+    }
 
+
+    @Test
+    public void testCollectionContains(){
+        setup();
+        data.addCollection(COLLECTION_NAME1);
+        assertTrue(data.containsCollection(COLLECTION_NAME1));
+
+        data.addCollection(COLLECTION_NAME1);
+        data.addCollection(COLLECTION_NAME2);
+        data.addCollection(COLLECTION_NAME3);
+
+        assertTrue(data.containsCollection(COLLECTION_NAME1));
+        assertTrue(data.containsCollection(COLLECTION_NAME2));
+        assertTrue(data.containsCollection(COLLECTION_NAME3));
+        assertFalse(data.containsCollection(COLLECTION_NAME4));
+    }    
+    @Test
+    public void testAddCollection(){
+        setup();
+        data.addCollection(COLLECTION_NAME1);
+        assertTrue(data.containsCollection(COLLECTION_NAME1));
+    }
     @Test
     public void testAddAndRemoveCollection(){
         setup();
         data.addCollection(COLLECTION_NAME1);
+        assertTrue(data.containsCollection(COLLECTION_NAME1));
+
         data.addCollection(COLLECTION_NAME1);
         data.addCollection(COLLECTION_NAME2);
         data.addCollection(COLLECTION_NAME3);
@@ -89,16 +128,38 @@ public class UserDataTest extends TestConstants {
     }
 
     @Test
+    public void testGetCollectionTitles(){
+        setup();
+        data.addCollection(COLLECTION_NAME1);
+        data.addCollection(COLLECTION_NAME2);
+        data.addCollection(COLLECTION_NAME3);
+        data.addCollection(COLLECTION_NAME4);
+
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add(COLLECTION_NAME1);
+        arr.add(COLLECTION_NAME2);
+        arr.add(COLLECTION_NAME3);
+        arr.add(COLLECTION_NAME4);
+        assertLinesMatch(arr, data.getCollectionTitles());
+    }
+
+    @Test
+    public void testAddToCollection(){
+        setup();
+        data.addCollection(COLLECTION_NAME1);
+        assertFalse(data.collectionContains(TEST_MOVIE_ID, COLLECTION_NAME1));
+
+        data.addToCollection(TEST_MOVIE_ID, COLLECTION_NAME1);
+        assertTrue(data.collectionContains(TEST_MOVIE_ID, COLLECTION_NAME1));
+
+        data.removeFromCollection(TEST_MOVIE_ID, COLLECTION_NAME1);
+        assertFalse(data.collectionContains(TEST_MOVIE_ID, COLLECTION_NAME1));
+    }
+    @Test
     public void testAddAndRemoveFromCollection(){
         setup();
         data.addCollection(COLLECTION_NAME1);
-        // Movie m = new Movie.MovieBuilder()
-        //     .movieId(TEST_MOVIE_ID)
-        //     .movieName(TEST_TITLE)
-        //     .releaseDate(TEST_RELEASE_DATE)
-        //     .averageRating(TEST_AVG_RATING)
-        //     .build(); 
-                   
+
         data.addToCollection(TEST_MOVIE_ID, COLLECTION_NAME1);
         assertTrue(data.collectionContains(TEST_MOVIE_ID, COLLECTION_NAME1));
 
